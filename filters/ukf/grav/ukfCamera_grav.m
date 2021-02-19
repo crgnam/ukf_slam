d290198003f57ad92cf30afce855a6add3a8e461
Chris Gnam <crgnam@buffalo.edu>
2021-02-19 15:34:25 -0700

    templates for ukf models added for gravity only case.  NOT FILLED OUT

---
+++ filters/ukf/grav/ukfCamera_grav.m
@@ -0,0 +1,9 @@
+function [predicted_measurement] = ukfCamera_grav(~,X,satellite,avails)
+    r = X(1:3);
+%     v = X(4:6);
+    [imagePoints,~] = satellite.camera.worldToImage(reshape(X(7:end),[],3)',...
+                                                    satellite.rotmat,r);
+    imagePoints = -imagePoints;
+    predicted_measurement = [imagePoints(1,:)'; imagePoints(2,:)'];
+    predicted_measurement = predicted_measurement(avails);
+end
